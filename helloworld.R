01de01ee570c68c29529ec3d224af8b753c29d58
atanaltay <atanaltay@gmail.com>
2025-09-29 14:00:14 +0300

    delete test

--- helloworld.R
+++
@@ -1,8 +0,0 @@
-print("hello world")
-# This is a simple R script that prints "hello world" to the console.
-# To run this script, save it as helloworld.R and execute it in an R environment.
-# Example command to run the script: Rscript helloworld.R
-
-age <- 25
-print(paste("Ali is",age,"years old"))
-
